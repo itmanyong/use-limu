d586c54654c4bddb5da6ba4543473080b6c05df6
cxy <itmanyong@gmail.com>
2021-11-10 11:09:06 +0800

    优化setState

--- src/use-limu-reducer.js
+++ src/use-limu-reducer.js
@@ -1,50 +1,15 @@
-/*
- * @Author: itmanyong
- * @Date: 2021-07-19 17:26:34
- * @LastEditTime: 2021-08-12 18:03:24
- * @LastEditors: itmanyong
- * @Description: 
- * @FilePath: \use-limu\src\use-limu-reducer.js
- * ___
- */
-/*
- * @Author: itmanyong
- * @Date: 2021-07-09 09:55:22
- * @LastEditTime: 2021-07-09 11:40:43
- * @LastEditors: itmanyong
- * @Description: 
- * @FilePath: \use-limu\example\src\libs\use-limu-reducer.js
- * ___
- */
 import React from 'react';
 import useLimu from './use-limu-state';
 /**
- * useReducer的limu版本,与react-useReducer的唯一区别是无需返回数据,直接修改,详见下面example
- * @param {function} reducer 事件分发函数,三个参数(state,action,payload)
- * @param {any} initState 初始化数据
- * @returns 【 state , dispatch ]
- * @example
- *    function reducer(state, action,payload) {
- *      switch (action) {
- *        case "increment":
- *          state.count++;
- *          break;
- *        case "decrement":
- *          state.count--;
- *          break;
- *      }
- *    }
+ * useReducer的limu版本
+ * @param {function} reducer dispatch回调
+ * @param {object|function} initState 初始值,对象或返回对象的函数
+ * @returns [state,diaptch]
  */
 export default function useLimuReducer(reducer, initState = null) {
-	const [state, setState] = useLimu(initState);
+    const [state, setState] = useLimu(initState);
 
-	return [
-		state,
-		React.useCallback(
-			(action, payload) => {
-				setState((draft) => reducer(draft, action, payload));
-			},
-			[state],
-		),
-	];
+    const dispatch = React.useRef((action, payload) => setState(draft => reducer(draft, action, payload)));
+
+    return [state, dispatch.current];
 }
